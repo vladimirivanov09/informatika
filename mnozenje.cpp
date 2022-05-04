067e1c4dda3d33baf7029123484063b40b74ec66
Vladimir Ivanov <vladimirivanov31072009@gmail.com>
2022-05-04 22:41:58 +0200

    Domasno od Vladimir Ivanov 7g

--- mnozenje.cpp
+++ mnozenje.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+   int broj1, broj2;
+    int proizvod;
+    cout << "Vnesi go brojot 1\n";
+    cin >> broj1;
+    cout << "Vnesi go brojot 2\n";
+    cin >> broj2;
+    proizvod = broj1 * broj2;
+    cout << proizvod;
 
     return 0;
 }
